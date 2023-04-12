2f4a398c99e8e604c56e7af1708bec091b44e024
Briana Grant <66385889+grantbriana@users.noreply.github.com>
2023-04-12 14:22:16 -0500

    Add files via upload

--- Lab08.ino
+++ Lab08.ino
@@ -1,18 +1,19 @@
+//Simple program for the implementation of a Temperature & Humidity Sensor utilizing Arduino & ESP32 with DHT11/DHT22
 #include "DHT.h"
-#define DHTPIN D2
-#define DHTTYPE DHT11
+#define DHTPIN D2 //Temp. sensor
+#define DHTTYPE DHT11 //Humidity sensor
 DHT dht(DHTPIN,DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600);//Used for communication between the Arduino board and a computer or other devices.
   dht.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float h = dht.readHumidity();  //Reads Humidity
+  float t = dht.readTemperature();  //Reads temperature
 
   Serial.print("Humidity: ");
   Serial.print(h);
